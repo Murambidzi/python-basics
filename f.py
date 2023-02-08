f3daaef933223a1b8a9620ba1e93ffeceadb9690
Tonderai Murambidzi <murambidzitonde@gmail.com>
2023-02-08 12:59:25 +0200

    added student details

--- f.py
+++ f.py
@@ -1,2 +1,11 @@
 def name():
     print("Hello World")
+
+def student_details(studentid, name, surname, programme):
+    d = "these are student details" + studentid
+    d += name
+
+    d += surname
+
+    d += programme
+    return d
